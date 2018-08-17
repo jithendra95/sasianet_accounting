dcdf0f5160f3db24c5f785245242783469fce19b
jithendra <jithendra@sasianet.com>
2018-08-17 19:00:32 +0530

    timer added

--- src/pages/add-memo/add-memo.ts
+++ src/pages/add-memo/add-memo.ts
@@ -9,6 +9,7 @@ import { InterfaceProvider } from '../../providers/interface/interface';
 import { Storage } from '@ionic/storage';
 
 import {MemoAutoCompleteProvider} from '../../providers/memo-auto-complete/memo-auto-complete';
+import { Observable } from 'rxjs';
 /**
  * Generated class for the AddMemoPage page.
  *
@@ -25,12 +26,27 @@ export class AddMemoPage {
 
   mode ='New';
   memo = {} as Memo;
+  timeDiffH=0;
+  timeDiffMM=0;
+  timeDiffSS=0;
+
+  timeStart=0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public storage:Storage,public http: Http,public intProv:InterfaceProvider,
               public memoService:MemoAutoCompleteProvider) {
 
+              this.memo.Start_time=(new Date()).toLocaleTimeString();
+              this.timeStart=new Date().getTime();
+              this.memo.End_time=(new Date()).toLocaleTimeString();
+
               
+              
+              
+              Observable.interval(1*60).subscribe(x=>{
+                this.memo.End_time=(new Date()).toLocaleTimeString();
+                this.timeDiffSS=Math.round(((new Date().getTime() - this.timeStart)/1000));
+              })
   }
 
   ionViewDidLoad() {
